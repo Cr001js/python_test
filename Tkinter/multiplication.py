48c9449df58b24ed7bd0752d08c4fc37b8418ad7
Cr001js <veladimir.viliss@gmail.com>
2023-03-07 16:12:25 +0330

    7\3\2023 changes (multiplication code added)

---
+++ Tkinter/multiplication.py
@@ -0,0 +1,31 @@
+# This code takes two numbers from the user and multiplies them together :)))
+# I have written this code with the help of Tkinter (=^ェ^=)
+
+from tkinter import *
+
+window = Tk()
+window.title('multiplication')
+
+def multiplication():
+    n1 = num1.get()
+    n2 = num2.get()
+    n1 = int(n1)
+    n2 = int(n2)
+    m = n1 * n2
+    multiplication_lb.config(text = 'Multiplied number: %i ' % m, fg = 'green')
+
+Label(window, text = 'enter number 1').pack()
+num1 = Entry(window)
+num1.pack()
+
+Label(window, text = 'enter number 2').pack()
+num2 = Entry(window)
+num2.pack()
+
+Button(window, text = 'do multiplication', command= multiplication).pack()
+
+multiplication_lb = Label(window, text = 'no Multiplied number', fg = 'red')
+multiplication_lb.pack()
+
+window.geometry('200x200')
+window.mainloop()
