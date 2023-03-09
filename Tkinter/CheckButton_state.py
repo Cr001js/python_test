b37dad27f0fd90bc74bb759bc3d27b138c74d3d6
Cr001js <veladimir.viliss@gmail.com>
2023-03-09 14:08:55 +0330

    93\2023 changes

--- Tkinter/CheckButton_state.py
+++ Tkinter/CheckButton_state.py
@@ -5,7 +5,11 @@ window = Tk()
 window.title('Checkbutton')
 
 def get_state() :
-    print('state is: ', male_val.get())
+    s = male_val.get()
+    if s == 1:
+        lb.config(text = 'online', fg = 'green')
+    elif s == 0:
+        lb.config(text = 'offline', fg = 'red')
 
 
 
@@ -14,5 +18,7 @@ Checkbutton(window, text = 'connection', variable = male_val).pack()
 
 Button(window, text = 'Show State', command = get_state).pack()
 
+lb = Label(window, text = '', fg = 'red')
+lb.pack()
 window.geometry('300x300')
 window.mainloop()
