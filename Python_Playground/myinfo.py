a0cc9f829b214f001f41d915d652b419f62d7f3a
Veladimir.viliss <96004456+Cr001js@users.noreply.github.com>
2023-03-19 16:12:23 +0330

    19/3/2023 changes

--- Python_Playground/myinfo.py
+++ Python_Playground/myinfo.py
@@ -1,7 +1,6 @@
 # this is my info ( o˘◡˘o) ┌iii┐
 #today i don't have time for python:(
 
-print('hello git')
 print('''
     hello everyone my name is mmd and im learning python =)
     im new in python
